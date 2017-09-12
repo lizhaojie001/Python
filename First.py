ba20420affa3fe4435c9d0f2d58f9bc2fa1ad0a5
lizhaojie001 <252487266@qq.com>
2017-09-12 20:06:51 +0800

    第一次提交

---
+++ First.py
@@ -0,0 +1,12 @@
+
+# First Steps Towards Programming
+# print u'Hello world'
+
+# a, b= 0, 1
+# while b < 10:
+#     print('b='+str(b))
+#     a, b = b, a + b
+#     print 'a='+str(a)+'\n'
+#
+# i = 256*256
+print('the value of i is ',i)
